fd377e1308f2d255df64e498b4b9c7e19a0c0585
shaikjanipasha <32264707+shaikjanipasha@users.noreply.github.com>
2025-04-25 12:49:32 +0530

    initial commit setup
    initial commit setup

---
+++ src/main/java/com/vtx/jobscheduler/JobSchedulerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vtx.jobscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSchedulerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSchedulerServiceApplication.class, args);
+	}
+
+}
